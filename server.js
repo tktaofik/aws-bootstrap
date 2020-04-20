5c6696098e6cc4835b283d8d93c401d4448a9890
Taofiki Yussuff <tk.yussuff@solace.com>
2020-04-19 20:06:02 -0400

    Update server.js

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const { hostname } = require("os");
 const http = require("http");
 
-const message = `Hello World from ${hostname()}\n`;
+const message = `Test: Hello World from  ${hostname()}\n`;
 const port = 8080;
 
 const server = http.createServer((req, res) => {
